82dc4bfeafc5da2c68a335d4f3cf523a1251dad4
Iman Mk <rimanmk@gmail.com>
2015-12-04 19:19:07 -0500

    Initial Commit

---
+++ Lanocast/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Lanocast
+//
+//  Created by Iman Mk R on 12/4/15.
+//  Copyright © 2015 Lanocast. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
