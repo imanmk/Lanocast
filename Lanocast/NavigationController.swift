cfccce4382419b2061021d2a5e34fc4b02fcafdd
Iman Mk <rimanmk@gmail.com>
2016-01-22 20:12:48 -0500

    update gallery UI with constraints

---
+++ Lanocast/NavigationController.swift
@@ -0,0 +1,21 @@
+//
+//  NavigationController.swift
+//  Lanocast
+//
+//  Created by Iman Mk R on 12/10/15.
+//  Copyright © 2015 Lanocast. All rights reserved.
+//
+
+import UIKit
+
+class NavigationController: UINavigationBar {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Lanocast/TabBarViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TabBarControllerViewController.swift
+//  Lanocast
+//
+//  Created by Iman Mk R on 12/10/15.
+//  Copyright © 2015 Lanocast. All rights reserved.
+//
+
+import UIKit
+
+class TabBarControllerViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
